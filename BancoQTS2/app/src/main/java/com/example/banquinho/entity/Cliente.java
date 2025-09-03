a5ef8ae70a4cbbca6f9184d7340d98a79b9390a8
GiovannaAdantas <giovanna.andrade0112@gmail.com>
2025-09-03 17:34:22 -0300

    Banco com 1 teste

---
+++ BancoQTS2/app/src/main/java/com/example/banquinho/entity/Cliente.java
@@ -0,0 +1,31 @@
+package com.example.banquinho.entity;
+
+public class Cliente {
+	
+	private String name;
+	private int id;
+	private long cpf;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public long getCpf() {
+		return cpf;
+	}
+	public void setCpf(long cpf) {
+		this.cpf = cpf;
+	}
+
+	
+	
+
+}
